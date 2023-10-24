d26262e586f35a4ff63559b60095aaa70601bf5a
AbyEricTety <145768645+AbyEricTety@users.noreply.github.com>
2023-10-24 03:00:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for appointment booking (example)
+document.querySelector('.appointment-btn').addEventListener('click', () => {
+    alert('You clicked the appointment button. You can add your booking functionality here.');
+});
